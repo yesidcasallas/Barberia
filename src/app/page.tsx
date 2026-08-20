878b008ccaf3d470ad711056fafe9a010c5d1ebe
Yesid Casallas <casallas912@gmail.com>
2026-08-20 00:11:58 -0500

    Logros: Ajustes en el build problemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/header/Header";
-import Footer from "@/components/footer/footer";
-import HeroSection from "@/components/secctions/heroSection"
+import Footer from "@/components/footer/Footer";
+import HeroSection from "@/components/secctions/HeroSection";
 
 export default function Home() {
   return (
